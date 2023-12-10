9e715fa1d1025efdc5eef126c707663b8d544eb5
Shivam Patel <87613778+ShivamPatel-24@users.noreply.github.com>
2023-12-10 10:13:43 -0500

    reduce redundancy and fix minor issues

--- game.py
+++ game.py
@@ -1,91 +1,53 @@
-
 import logic
 
 # global running variable
 RUN = True
-P_CMD = False
 
 # Driver code
 if __name__ == '__main__':
-	
-# calling start_game function
-# to initialize the matrix
-	mat = logic.start_game()
+    # calling start_game function
+    # to initialize the matrix
+    mat = logic.start_game()
 
-while(RUN):
+    while RUN:
+        # taking the user input for the next step
+        x = input(":")
 
-	# taking the user input
-	# for next step
-	P_CMD = False
-	x = input(":")
+        # Define a dictionary to map user input to functions
+        move_actions = {
+            'W': logic.move_up,
+            'S': logic.move_down,
+            'A': logic.move_left,
+            'D': logic.move_right
+        }
 
-	# we have to move up
-	if(x == 'W' or x == 'w'):
+        # Check if the input is a valid key
+        if x.upper() in move_actions:
+            # Call the corresponding move function
+            mat, flag = move_actions[x.upper()](mat)
 
-		# call the move_up function
-		mat, flag = logic.move_up(mat)
+            # get the current state and print it
+            status = logic.get_current_state(mat)
+            print(status)
 
-		# get the current state and print it
-		status = logic.get_current_state(mat)
-		print(status)
+            # if the game is not over, continue and add a new two
+            if status == 'GAME NOT OVER':
+                logic.add_new_2(mat)
+            else:
+                RUN = False  # Set RUN to False when the game is over
 
-		# if game not over then continue
-		# and add a new two
-		if(status == 'GAME NOT OVER'):
-			logic.add_new_2(mat)
+        # Show Commands
+        elif x.upper() == 'P':
+            logic.show_commands()
 
-		# else break the loop
-		else:
-			break
+        # exit game
+        elif x.upper() == 'X':
+            print("Exiting Game")
+            RUN = False
 
-	# the above process will be followed
-	# in case of each type of move
-	# below
+        else:
+            print("Invalid Key Pressed")
 
-	# to move down
-	elif(x == 'S' or x == 's'):
-		mat, flag = logic.move_down(mat)
-		status = logic.get_current_state(mat)
-		print(status)
-		if(status == 'GAME NOT OVER'):
-			logic.add_new_2(mat)
-		else:
-			break
-
-	# to move left
-	elif(x == 'A' or x == 'a'):
-		mat, flag = logic.move_left(mat)
-		status = logic.get_current_state(mat)
-		print(status)
-		if(status == 'GAME NOT OVER'):
-			logic.add_new_2(mat)
-		else:
-			break
-
-	# to move right
-	elif(x == 'D' or x == 'd'):
-		mat, flag = logic.move_right(mat)
-		status = logic.get_current_state(mat)
-		print(status)
-		if(status == 'GAME NOT OVER'):
-			logic.add_new_2(mat)
-		else:
-			break
-
-	# Show Commands
-	elif(x == 'P' or x == 'p'):
-		logic.show_commands()
-		P_CMD = True
-
-	# exit game
-	elif(x == 'X' or x == 'x'):
-		print("Exiting Game")
-		RUN = False
-
-	else:
-		print("Invalid Key Pressed")
-
-	# print the matrix after each
-	# move.
-	if not P_CMD:
-		logic.draw_grid(mat)
+        # print the matrix after each move.
+        if x.upper() not in ('P', 'X') and status != 'GAME OVER':
+            logic.draw_grid(mat)
